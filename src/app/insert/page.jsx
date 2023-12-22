d76dc4a8d1b8088503e728cf9bf7dbbc23d30530
Ririena <nerllysurina@gmail.com>
2023-12-22 17:25:35 +0700

    Basic CRUD

--- src/app/insert/page.jsx
+++ src/app/insert/page.jsx
@@ -1,41 +1,102 @@
-'use client'
-import React, { useState } from 'react';
-import { supabase } from '../config/supabase';
+"use client";
+import React, { useState, useEffect } from "react";
+import { supabase } from "../config/supabase";
+
 
 const AddRowForm = () => {
+  const [updateNamaUser, setUpdatedNamaUser] = useState("");
+  const [data, setData] = useState([]);
   const [formData, setFormData] = useState({
-    nama_user: '',
-    jenis_user: '',
+    nama_user: "",
+    jenis_user: "",
   });
+
+  const fetchData = async () => {
+
+    try {
+      const { data, error } = await supabase.from("profiles").select("*");
+      if (error) {
+        console.error("Error fetching data:", error.message);
+      } else {
+        setData(data);
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    // Ambil data saat komponen dimuat
+    fetchData();
+
+    // Set interval untuk HTTP polling setiap beberapa detik
+    const interval = setInterval(fetchData, 2000); // Ganti sesuai kebutuhan polling interval (dalam milidetik)
+
+    // Bersihkan interval saat komponen dilepas (unmount)
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  async function updateData(id) {
+    try{
+
+      const { error } = await supabase
+      .from("profiles")
+      .update({ nama_user: updateNamaUser })
+      .eq("id", id);
+      
+      if(error) {
+        console.error("Error", error.message)
+      } else{
+        console.log("ROW DI UPDATE", updateNamaUser)
+        fetchData()
+        setUpdatedNamaUser("")
+      }
+    } catch(error) {
+      console.error("error", error.message)
+    }
+  }
+  async function deletes(id) {
+    try {
+      const { error } = await supabase.from("profiles").delete().eq("id", id);
+
+      if (error) {
+        console.error("Error", error.message);
+      } else {
+        console.log("Berhasil Delete", data);
+        fetchData();
+      }
+    } catch (error) {
+      console.error("error", error.message);
+    }
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const tableName = 'profiles';
+    const tableName = "profiles";
 
     try {
       // Menambahkan baris ke tabel
-      const { data, error } = await supabase
-        .from(tableName)
-        .insert([formData]);
+      const { data, error } = await supabase.from(tableName).insert([formData]);
 
       if (error) {
-        console.error('Error inserting row:', error.message);
+        console.error("Error inserting row:", error.message);
       } else {
-        console.log('Row inserted successfully:', data);
+        console.log("Row inserted successfully:", data);
         // Reset formulir setelah berhasil menambahkan baris
         setFormData({
-          nama_user: '',
-          jenis_user: '',
-          // Reset kolom-kolom lain sesuai kebutuhan
+          nama_user: "",
+          jenis_user: "",
         });
       }
     } catch (error) {
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
     }
   };
 
@@ -60,9 +121,26 @@ const AddRowForm = () => {
             onChange={handleChange}
           />
         </label>
-        {/* Tambahkan input untuk kolom-kolom lain sesuai kebutuhan */}
         <button type="submit">Tambahkan Baris</button>
       </form>
+      <div>
+        <h1>Data</h1>
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              {JSON.stringify(item)}
+              <button onClick={() => deletes(item.id)}>Delete</button>
+              <input
+                type="text"
+                name="updateNamaUser"
+                value={updateNamaUser}
+                onChange={(e) => setUpdatedNamaUser(e.target.value)}
+              ></input>
+            <button onClick={() => updateData(item.id)}>Update</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
